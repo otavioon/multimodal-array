203fa3828b7e4121ebbec50baa24dc00cceb90b0
Otavio Napoli <otavio.napoli@gmail.com>
2022-09-09 17:18:44 +0100

    Added apply to all and per window

--- multimodal/__init__.py
+++ multimodal/__init__.py
@@ -1,2 +1,2 @@
 from .array import MultiModalArray, apply_multimodal_array_func
-from .dataframe import MultiModalDataframe, apply_multimodal_dataframe_func
+from .dataframe import MultiModalDataframe, apply_func, apply_func_per_window
